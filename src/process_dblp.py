90538a5876e189c2b474bc4976f2b00df7ffd0d2
James Yu <james.yu.school@gmail.com>
2022-01-13 22:31:17 -0800

    continued work on process_dblp
    Had issues with processing some of the blocks - currently WIP as it doesn't work on the entire dataset atm

--- src/process_dblp.py
+++ src/process_dblp.py
@@ -12,13 +12,22 @@ def process_json(inpath):
     Process the inpath of DBLP v13 dataset
     Outputs a list of dictionaries (each dictionary is a paper)
 
+    Only care about: title, year, keywords, abstract
+    When we encounter one of these tags, we want to keep processing lines until we reach an invalid tag
+
     >>> process_json('../data/dblpv13.json')
     """
     #count = 0
     data = []
     first_line = True
+    tags = ['title', 'year', 'keywords', 'abstract', 'fos']
+    invalid_tags = ['"venue" :', '"_id" :', '"type" :', '"raw" :', '"raw_zh" :',
+                    '"ncitation" :', '"page_start" :', '"page_end" :',
+                    '"lang" :', '"volume" :', '"issue" :', '"issn" :',
+                    '"isbn" :', '"doi" :']
+    valid_tag = True
 
-    with open(inpath) as f:
+    with open(inpath, 'r', encoding='utf-8') as f:
         content = ''
         # Keeps track of the start of processing a block (individual paper)
         start_ind = False
@@ -26,6 +35,7 @@ def process_json(inpath):
             # Indicates start of block
             if line == '{ \n':
                 start_ind = True
+
             # Indicates end of block
             if line == '},\n':
                 # Doesn't process the first block (only contains info about structure)
@@ -36,18 +46,31 @@ def process_json(inpath):
                 # Cleans up formatting of block
                 content += '}\n'
                 content = content.replace(' :', ':').replace(',', ', ')
-                data.append(json.loads(content))
+                try:
+                    json.loads(content)
+                except:
+                    print('--------------- ISSUE HAS OCCURRED --------------')
+                    break
+                #data.append(json.loads(content))
+                print('----------------------')
                 start_ind = False
                 content = ''
 
             if start_ind:
                 # Cleans up line to prevent issues with json.loads()
                 line = line.strip()
+                if any([tag in line for tag in tags]):
+                    valid_tag = True
+                else:
+                    valid_tag = False
                 if 'NumberInt' in line:
                     line = line.replace('NumberInt(', '')
                     line = line.strip()[:-2] + ','
                 if line[-3:] == ': ,':
                     line = line.replace(': ,', ': null')
+                if line == '"type" : 1,':
+                    continue
+                print(line)
                 content += line
 
             #count += 1
