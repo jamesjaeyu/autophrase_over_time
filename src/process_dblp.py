2a6b67de581e434335e71336f959c62bab4745d2
James Yu <james.yu.school@gmail.com>
2022-01-15 20:45:01 -0800

    continued fixing of process_dblp

--- src/process_dblp.py
+++ src/process_dblp.py
@@ -17,21 +17,24 @@ def process_json(inpath):
 
     >>> process_json('../data/dblpv13.json')
     """
-    #count = 0
+    count = 0
     data = []
     first_line = True
-    tags = ['title', 'year', 'keywords', 'abstract', 'fos']
+    tags = ['"title"', '"year"', '"keywords"', '"abstract"', '"fos"']
     invalid_tags = ['"venue" :', '"_id" :', '"type" :', '"raw" :', '"raw_zh" :',
-                    '"ncitation" :', '"page_start" :', '"page_end" :',
+                    '"n_citation" :', '"page_start" :', '"page_end" :',
                     '"lang" :', '"volume" :', '"issue" :', '"issn" :',
-                    '"isbn" :', '"doi" :']
+                    '"isbn" :', '"doi" :', '"pdf" :', '"url" :', '"references" :']
     valid_tag = True
+    num_papers = 0
 
     with open(inpath, 'r', encoding='utf-8') as f:
-        content = ''
+        content = '{'
         # Keeps track of the start of processing a block (individual paper)
         start_ind = False
         for line in f:
+            # TODO: need to fix issue with paper #3408
+
             # Indicates start of block
             if line == '{ \n':
                 start_ind = True
@@ -41,28 +44,48 @@ def process_json(inpath):
                 # Doesn't process the first block (only contains info about structure)
                 if first_line:
                     first_line = False
-                    content = ''
+                    content = '{'
                     continue
                 # Cleans up formatting of block
                 content += '}\n'
                 content = content.replace(' :', ':').replace(',', ', ')
+                # Prevents issue with additional comma at end of dict
+                if content[-4:-2] == ', ':
+                    content = content[:-4] + '}\n'
+                # Fix for issue with double beginning brackets
+                if content[:2] == '{{':
+                    content = content[1:]
+
+                # for testing purposes
+                num_papers += 1
+                print(str(num_papers) + ' papers processed')
                 try:
                     json.loads(content)
                 except:
+                    print(content)
                     print('--------------- ISSUE HAS OCCURRED --------------')
                     break
                 #data.append(json.loads(content))
                 print('----------------------')
+
                 start_ind = False
-                content = ''
+                content = '{'
 
             if start_ind:
                 # Cleans up line to prevent issues with json.loads()
                 line = line.strip()
+                print(line)
                 if any([tag in line for tag in tags]):
+                    #line = line
                     valid_tag = True
-                else:
+                #elif any([tag in invalid_tags for tag in tags]):
+                # TODO: fix issue where '"text : text' causes this case to be True
+                #       as it causes scanning to stop too early
+                elif ' : ' in line:
                     valid_tag = False
+                if not valid_tag:
+                    continue
+
                 if 'NumberInt' in line:
                     line = line.replace('NumberInt(', '')
                     line = line.strip()[:-2] + ','
@@ -70,12 +93,8 @@ def process_json(inpath):
                     line = line.replace(': ,', ': null')
                 if line == '"type" : 1,':
                     continue
-                print(line)
+                #print(line)
                 content += line
-
-            #count += 1
-            #if count > 200:
-            #    break
     return data
 
 
