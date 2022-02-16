d4c1d1aed64d51fc251a39b2824210f6e43a39cc
James Yu <james.yu.school@gmail.com>
2022-02-16 00:19:49 -0800

    added process_v10 function for grouped years

--- src/process_dblp_v10.py
+++ src/process_dblp_v10.py
@@ -7,6 +7,7 @@ import pandas as pd
 import json
 import time
 import re
+import os
 
 def print_stats(time, papers, issues, empty, year):
     print(str(time) + ' seconds has elapsed since start of function')
@@ -27,7 +28,9 @@ def process_v10_txt(infolder):
     """
     start = time.time()
     filepaths = [infolder + '/dblp-ref-' + str(num) + '.json' for num in range(4)]
-    outfolder = infolder + '/temp/'
+    outfolder = infolder + '/txt/'
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
 
     num_no_abstract, num_papers, num_issues, num_year = 0, 0, 0, 0
 
@@ -65,14 +68,66 @@ def process_v10_txt(infolder):
     end = time.time()
     time_elapsed = end - start
     print_stats(time_elapsed, num_papers, num_issues, num_no_abstract, num_year)
-    return
 
 
 def process_v10_txt_grouped(infolder):
     """
     Processes DBLP v10 but with years grouped by 5
+
+    >>> process_v10_txt_grouped('../data/dblp-v10')
     """
-    return
+    start = time.time()
+    # Creates dictionary mapping for each year's new year-range category
+    mapping = {}
+    for i in range(1950, 2019, 5):
+        if i == 2015:
+            cat = '2015-2017'
+        else:
+            cat = str(i) + '-' + str(i+4)
+        for j in range(i, i+5):
+            mapping[j] = cat
+
+    # Processes input files
+    filepaths = [infolder + '/dblp-ref-' + str(num) + '.json' for num in range(4)]
+    outfolder = infolder + '/txt_grouped/'
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
+    num_no_abstract, num_papers, num_issues, num_year = 0, 0, 0, 0
+    for fp in filepaths:
+        file = open(fp)
+        for line in file:
+            num_papers += 1
+            try:
+                data = json.loads(line)
+                year = data['year']
+                if year < 1950 or year > 2018:
+                    num_year += 1
+                    continue
+                year = mapping[year]
+                outpath = outfolder + str(year) + '.txt'
+                outfile = open(outpath, 'a')
+                if 'abstract' in data.keys():
+                    if len(data['abstract']) == 0:
+                        num_no_abstract += 1
+                        continue
+                    abstract = data['abstract']
+                    abstract = re.sub(r'[^A-Za-z0-9- ]+', '', abstract)
+                    outfile.write(abstract + '\n')
+                else:
+                    num_no_abstract += 1
+                    continue
+                if 'title' in data.keys():
+                    title = data['title']
+                    title = re.sub(r'[^A-Za-z0-9- ]+', '', title)
+                    outfile.write(title + '\n')
+                else:
+                    continue
+            except:
+                num_issues += 1
+        file.close()
+    end = time.time()
+    time_elapsed = end - start
+    print_stats(time_elapsed, num_papers, num_issues, num_no_abstract, num_year)
 
 
 def process_v10_csv(infolder):
@@ -84,6 +139,8 @@ def process_v10_csv(infolder):
     start = time.time()
     filepaths = [infolder + '/dblp-ref-' + str(num) + '.json' for num in range(4)]
     outfolder = infolder + '/csv/'
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
     years = set()
 
     num_no_abstract, num_no_title, num_papers, num_issues, num_year = 0, 0, 0, 0, 0
@@ -121,7 +178,6 @@ def process_v10_csv(infolder):
     end = time.time()
     time_elapsed = end - start
     print_stats(time_elapsed, num_papers, num_issues, num_no_abstract, num_year)
-    return
 
 
 def process_v10_txt_agg(infolder):
@@ -130,7 +186,10 @@ def process_v10_txt_agg(infolder):
     """
     start = time.time()
     filepaths = [infolder + '/dblp-ref-' + str(num) + '.json' for num in range(4)]
-    outpath = infolder + '/agg/content_FULL.txt'
+    outfolder = infolder + '/agg/'
+    outpath = outfolder + 'content_FULL.txt'
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
 
     num_no_abstract, num_papers, num_issues, num_year = 0, 0, 0, 0
 
@@ -167,4 +226,3 @@ def process_v10_txt_agg(infolder):
     end = time.time()
     time_elapsed = end - start
     print_stats(time_elapsed, num_papers, num_issues, num_no_abstract, num_year)
-    return
