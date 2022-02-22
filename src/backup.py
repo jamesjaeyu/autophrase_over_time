2d0500102d8e91e3224f36384a4661722eb854cd
James Yu <james.yu.school@gmail.com>
2022-02-22 02:23:52 -0800

    removed sort_jsons.py from data folder

--- src/backup.py
+++ src/backup.py
@@ -3,6 +3,10 @@ DSC180B
 Q2 Project
 Backup for any unused functions
 """
+import pandas as pd
+import time
+import re
+from glob import glob
 
 # NOTE: model_generation.py functions for processing phrasal segmentation results
 # Using pandas is far more efficient than reading through the .txt files line by line
@@ -187,3 +191,72 @@ def obtain_phrases_alt(infolder, threshold):
     df.to_csv(outpath)
     end = time.time()
     return end - start
+
+
+# NOTE: From sort_jsons.py
+import json
+
+#Clear existing files before sorting json files
+for x in range(1935,2020):
+	filename = str(x) + '.txt'
+	open(filename, 'w').close()
+
+#For use with the V10 DBLP database from https://www.aminer.org/citation
+#This data set is split into four .json files, which are imported in sequence
+#Note: this takes a long time to run, so it's possible to comment out multiple sections of the data set for quicker running and testing
+file = open('dblp-ref-0.json')
+for l in file:
+	data = json.loads(l)
+	year = data['year']
+	yeartxt = str(year) + ".txt"
+	writefile = open(yeartxt, "a", encoding="utf-8")
+	if 'abstract' in data.keys():
+		writefile.writelines(data['abstract'])
+		writefile.writelines("\n")
+	if 'title' in data.keys():
+		writefile.writelines(data['title'])
+		writefile.writelines("\n")
+file.close()
+
+file = open('dblp-ref-1.json')
+for l in file:
+	data = json.loads(l)
+	year = data['year']
+	yeartxt = str(year) + ".txt"
+	writefile = open(yeartxt, "a", encoding="utf-8")
+	if 'abstract' in data.keys():
+		writefile.writelines(data['abstract'])
+		writefile.writelines("\n")
+	if 'title' in data.keys():
+		writefile.writelines(data['title'])
+		writefile.writelines("\n")
+file.close()
+
+file = open('dblp-ref-2.json')
+for l in file:
+	data = json.loads(l)
+	year = data['year']
+	yeartxt = str(year) + ".txt"
+	writefile = open(yeartxt, "a", encoding="utf-8")
+	if 'abstract' in data.keys():
+		writefile.writelines(data['abstract'])
+		writefile.writelines("\n")
+	if 'title' in data.keys():
+		writefile.writelines(data['title'])
+		writefile.writelines("\n")
+file.close()
+
+file = open('dblp-ref-3.json')
+for l in file:
+	data = json.loads(l)
+	year = data['year']
+	yeartxt = str(year) + ".txt"
+	writefile = open(yeartxt, "a", encoding="utf-8")
+	if 'abstract' in data.keys():
+		writefile.writelines(data['abstract'])
+		writefile.writelines("\n")
+	if 'title' in data.keys():
+		writefile.writelines(data['title'])
+		writefile.writelines("\n")
+file.close()
+
